586898b7f8689320e4089f80ed4b7bf8744de3bf
Derik <derikpessoal@gmail.com>
2019-06-10 18:49:15 -0300

    Configuring rack_attack

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module OneBitTwitter
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    #Habilitando o Rack::Attack
+    config.middleware.use Rack::Attack
   end
 end
