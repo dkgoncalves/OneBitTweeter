e07078a748b889a7850b2346d1c7c19a54062f19
Derik <derikpessoal@gmail.com>
2019-06-15 15:00:02 -0300

    Creating tweet model and adding your relations. Creating spec.

---
+++ app/models/tweet.rb
@@ -0,0 +1,12 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  belongs_to :tweet_original, class_name: 'Tweet', optional: true
+
+  has_many :retweets, class_name: 'Tweet', foreign_key: 'tweet_original_id'
+
+  # Pode receber likes
+  acts_as_votable
+
+  validates :body, presence: true
+  validates :user_id, precense: true
+end
