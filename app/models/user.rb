783f2f971033452fe2518f589c9573a1dce66944
Derik <derikpessoal@gmail.com>
2019-06-15 15:00:11 -0300

    Adding relations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_length_of :password, maximum: 72, minimum: 8, allow_nil: true, allow_blank: false
+
+  validates :name, presence: true
+  validates :email, presence: true
+
+  # Pode dar Like
+  acts_as_voter
+  # Pode ser seguido
+  acts_as_followable
+  # Pode seguir
+  acts_as_follower
+  has_many :tweets, dependent: :destroy
 end
