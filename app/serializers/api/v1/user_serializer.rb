0e725b1ce5cd5bc7f4e4675585c2ce655f97cf0b
Derik <derikpessoal@gmail.com>
2019-06-20 15:46:33 -0300

    Adding photo to user_serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,8 +1,8 @@
 module Api
   module V1
-    class UserSerializer < ActiveModel::Serializer # >
+    class UserSerializer < ActiveModel::Serializer
       # Quais atributos vamos devolver no json
-      attributes :id, :name, :email, :tweets_count, :followers_count, :following_count
+      attributes :id, :name, :email, :tweets_count, :followers_count, :following_count, :photo
 
       # O object dentro do Serializer é o nosso próprio usuário
       def tweets_count
